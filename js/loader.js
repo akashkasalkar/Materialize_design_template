7fda60f17beb9ed60d3bb5c51afc1fe8ed5a9003
akashkasalkar <kasalkar16@gmail.com>
2020-05-30 17:03:03 +0530

    intial commit

---
+++ js/loader.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.modal').modal();
+	 $('.parallax').parallax();
+	 $('.sidenav').sidenav();
+	 $(".slider").slider({full_widt:true});
+	 $(".myreviews").carousel({
+		 numVisible:7,
+		 shift:55,
+		 padding:55
+	 });
+})
+function toggleModal(){
+	var instance=M.Modal.getInstance($('#modal3'));
+	instance.open();
+}
